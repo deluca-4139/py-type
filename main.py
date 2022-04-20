24651a8fbd8625b166dc81ea7f1396bdd2d64878
Nicole De Luca <ddeluca@oberlin.edu>
2022-04-19 19:46:47 -0700

    WPM/CPM calc
    Add WPM/CPM calculation and display upon completing a test.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from curtsies import fmtstr, FullscreenWindow, Input, FSArray
 from curtsies.fmtfuncs import *
 
@@ -30,15 +32,17 @@ def construct_fmtstr(in_string, out_string, w):
 if __name__ == "__main__":
     with FullscreenWindow() as window:
         with Input() as input_generator:
-            state = "title" # We use a state variable to track inputs/outputs depending on program state (title, typing, etc)
+            state = "title"     # We use a state variable to track inputs/outputs depending on program state (title, typing, etc)
+            start_time = 0      # Records the start time of a test. Updated when state switched and test begins.
+            end_time = 0        # Records the end time of a test. Updated when test is completed.
 
             a = FSArray(window.height, window.width)
 
             test_string = "Hello! This is intended to be a test string."
 
             # These hard-coded values will break with too small a terminal
-            a[1, 0] = ["Welcome to Py-ping Test.".center(window.width)]
-            a[5, 0] = ["            [c] ~ Start a Typeracer test."]
+            a[1, 0] = ["welcome to yet another python typing test.".center(window.width)]
+            a[5, 0] = ["            [c] ~ start a Typeracer test."]
 
             input_string = ""
 
@@ -59,6 +63,7 @@ if __name__ == "__main__":
 
                         a = FSArray(window.height, window.width)
                         a[(window.height // 2), 0:len(test_string)] = [construct_fmtstr(input_string, test_string, window.width)]
+                        start_time = time.perf_counter()
                         window.render_to_terminal(a)
 
                 elif state == "typing":
@@ -70,11 +75,14 @@ if __name__ == "__main__":
                         input_string += c
 
                     if input_string == test_string:
+                        end_time = time.perf_counter()
                         state = "done"
 
                         a = FSArray(window.height, window.width)
-                        a[1, 0] = ["Well done! You finished the test.".center(window.width)]
-                        a[5, 0] = ["            [c] ~ Start another Typeracer test."]
+                        a[1, 0]  = ["well done.~".center(window.width)]
+                        a[5, 0]  = [("wpm:    " + str(round(    (len(test_string)/5)    /   ((end_time-start_time)/60),  2))).center(window.width)]
+                        a[6, 0]  = [("cpm:    " + str(round(    (len(test_string))    /   ((end_time-start_time)/60),  2))).center(window.width)]
+                        a[10, 0] = ["            [c] ~ start another Typeracer test."]
                         window.render_to_terminal(a)
                     else:
                         a = FSArray(window.height, window.width)
@@ -88,4 +96,6 @@ if __name__ == "__main__":
                         input_string = ""
                         a = FSArray(window.height, window.width)
                         a[(window.height // 2), 0:len(test_string)] = [construct_fmtstr("", test_string, window.width)]
+
+
                         window.render_to_terminal(a)
